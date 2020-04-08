55f6fe68683e35fee60de4a311e480e1e3164d1d
gtpatchaya <gtpatchaya@gmail.com>
2020-04-08 21:29:16 +0700

    callmapreactJs

--- src/App.js
+++ src/App.js
@@ -1,26 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const nostra = window.nostra;
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            map: {},
+            loaded: true
+        };
+    }
+    componentDidMount() {
+        nostra.onready = () => {
+            nostra.config.Language.setLanguage(nostra.language.E);
+            this.initialize();
+        };
+    }
+    initialize = () => {
+        var baseMapCountry = "TH";
+        var map = new nostra.maps.Map("map", {
+            id: "mapTest",
+            logo: true,
+            scalebar: true,
+            basemap: "streetmap",
+            slider: true,
+            level: 15,
+            lat: 13.722944,
+            lon: 100.530449,
+            country: baseMapCountry
+        });
+        map.events.layerAddComplete = function (e) {
+            if (!this.state.loaded) {
+                //hideLoading();
+            }
+        };
+        map.events.load = () => {
+            //hideLoading();
+            this.setState({
+                loaded: false
+            });
+        };
+        this.setState({
+            map: map
+        });
+    }
+    render() {
+        return (
+            <div className="Mapsize" id="map"></div>
+        )
+    }
 }
-
 export default App;
